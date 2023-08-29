e04a26f77638523f20a86bb84c0fbce63665818f
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-08-29 11:58:49 +0200

    Delete extra file and add image path uploaded

--- src/main/java/com/onrender/themba/discovercareers/entity/Category.java
+++ src/main/java/com/onrender/themba/discovercareers/entity/Category.java
@@ -2,21 +2,27 @@ package com.onrender.themba.discovercareers.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.Length;
 
 @Entity
 @Table(name = "CAREER_CATEGORY")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
+    @Column(length = Length.LONG32)
     private String description;
     private String imagePath;
-    @OneToOne(cascade = CascadeType.ALL)
-    private CategoryFile categoryFile;
+    @Transient
+    public String getFileLocation(){
+        return "/uploads/"+ id + "/" + imagePath;
+    }
 }
