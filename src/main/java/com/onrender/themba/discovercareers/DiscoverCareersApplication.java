bcf49d1be88fc9b2c583d50ffca9d352ab93efa4
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-08-29 11:57:07 +0200

    Update DiscoverCareersApplication.java
    Delete All Uploaded Files On StartUp

--- src/main/java/com/onrender/themba/discovercareers/DiscoverCareersApplication.java
+++ src/main/java/com/onrender/themba/discovercareers/DiscoverCareersApplication.java
@@ -1,13 +1,28 @@
 package com.onrender.themba.discovercareers;
 
+import com.onrender.themba.discovercareers.config.StorageProperty;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileSystemUtils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperty.class)
 public class DiscoverCareersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoverCareersApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner init(){
+		return (args -> {
+			StorageProperty storageProperty = new StorageProperty();
+			Path root = Paths.get(storageProperty.getUploadsLocation());
+			FileSystemUtils.deleteRecursively(root.toFile());
+		});
+	}
 }
