108822ae80b780f675e5bbe17bbda74c31463f90
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-08-30 21:03:41 +0200

    Change Entity

--- src/main/java/com/onrender/themba/discovercareers/entity/CategoryEntity.java
+++ src/main/java/com/onrender/themba/discovercareers/entity/CategoryEntity.java
@@ -7,13 +7,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.Length;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "CAREER_CATEGORY")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public class Category {
+public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -21,6 +24,14 @@ public class Category {
     @Column(length = Length.LONG32)
     private String description;
     private String imagePath;
+
+    @OneToMany(
+            mappedBy = "category",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<CareerEntity> careers = new ArrayList<>();
+
     @Transient
     public String getFileLocation(){
         return "/uploads/"+ id + "/" + imagePath;
--- src/main/java/com/onrender/themba/discovercareers/repository/CategoryRepository.java
+++ src/main/java/com/onrender/themba/discovercareers/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package com.onrender.themba.discovercareers.repository;
 
-import com.onrender.themba.discovercareers.entity.Category;
+import com.onrender.themba.discovercareers.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
