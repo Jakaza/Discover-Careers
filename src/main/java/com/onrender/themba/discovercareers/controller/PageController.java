ac7ddb0a26b9935c4c5bb1ad4ca066d22654bcbe
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-08-30 14:20:48 +0200

    Implemente Pagination and Design for paging

--- src/main/java/com/onrender/themba/discovercareers/controller/PageController.java
+++ src/main/java/com/onrender/themba/discovercareers/controller/PageController.java
@@ -5,9 +5,13 @@ import com.onrender.themba.discovercareers.config.StorageProperty;
 import com.onrender.themba.discovercareers.entity.Category;
 import com.onrender.themba.discovercareers.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,6 +29,7 @@ import java.util.Optional;
 
 @Controller
 public class PageController {
+    private final int PAGESIZE = 6;
     @Autowired
     private CategoryRepository categoryRepository;
     private Path uploadsLocation;
@@ -33,16 +38,14 @@ public class PageController {
     }
     @GetMapping("/")
     private String index(Model model){
-        List<Category> categoryList = categoryRepository.findAll();
-        System.out.println(categoryList);
-        model.addAttribute("categoryList", categoryList);
-        return "index";
+        return findPagination(1, model);
     }
     @GetMapping("/new-category")
     private String newCategory(Model model){
         List<Category> categoryList = categoryRepository.findAll();
         System.out.println(categoryList);
         model.addAttribute("categoryList", categoryList);
+        model.addAttribute("totalNo", 10);
         return "new_category";
     }
     @PostMapping("/add-new-category")
@@ -86,11 +89,32 @@ public class PageController {
         return "redirect:/";
     }
     @GetMapping("/new-career")
-    private String newCareer(){
+    private String newCareer(Model model){
+
+
+        model.addAttribute("totalNo", 10);
         return "new_career";
     }
+
+
     @GetMapping("/testing")
     private String testing(){
         return "testing";
+    }
+
+    @GetMapping("/page/{pageNumber}")
+    private String findPagination(@PathVariable("pageNumber") int currentPage, Model model){
+        Pageable pageable = PageRequest.of(currentPage - 1, PAGESIZE);
+        Page<Category> page = categoryRepository.findAll(pageable);
+
+        int totalPage = page.getTotalPages();
+        long totalItems = page.getTotalElements();
+        List<Category> categories = page.getContent();
+
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalItems", totalItems);
+        model.addAttribute("categories", categories);
+        return "index";
     }
 }
